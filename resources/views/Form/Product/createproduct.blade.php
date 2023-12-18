6fef0f000cc05e194e48e7cd6d05fffb23945fed
nurulabsr <absar.ru@yandex.com>
2023-12-18 13:32:34 +0600

    In the create.blade.php, added a form in the main-content, that hole card and card-body, inside a body take  a form to creating insert data in the database

--- resources/views/Form/Product/createproduct.blade.php
+++ resources/views/Form/Product/createproduct.blade.php
@@ -1,6 +1,18 @@
 @extends('home.index')
 @section('ninja_default_section')
 
+<div class="main-content">
+   <div class="card-header">
+        <div class="card">
+                <div class="card-body">
+                    <form action="">
 
+                        
+                    </form>
+
+                </div>
+        </div>
+   </div>  
+</div>
 
 @endsection
