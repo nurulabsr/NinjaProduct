edf090d805208dbcca512a68c4a599f1eb1718ad
nurulabsr <absar.ru@yandex.com>
2023-12-19 15:11:15 +0600

    In image validation, added 'ninja_thumnail_image' => ['required', 'max:2048', 'file', 'image', 'mimes:png,jpg', 'mimetypes:image/jpg,image/png', 'regex:/^(.*)(\.jpg|\.png)$/i'] and other filleds

--- app/Http/Controllers/NinjaProductController.php
+++ app/Http/Controllers/NinjaProductController.php
@@ -30,12 +30,17 @@ class NinjaProductController extends Controller
     {
         $product = New Product();
         
-        $request->validate([  //'regex:/^[a-zA-Z0-9$]+$/', 'regex:/^[a-zA-Z]+$/'
+        $request->validate([  //'regex:/^[a-zA-Z0-9$]+$/', 'regex:/^[a-zA-Z]+$/' , for image 'dimensions:max_width=1920,max_height=1080', 
+            
+            'ninja_thumnail_image' => ['required', 'max:2048', 'file', 'image', 'mimes:png,jpg', 'mimetypes:image/jpg,image/png', 'regex:/^(.*)(\.jpg|\.png)$/i'],
+            'carousel_image_1' => ['required', 'max:2048', 'file', 'image', 'mimes:png,jpg', 'mimetypes:image/jpg,image/png', 'regex:/^(.*)(\.jpg|\.png)$/i'],
+            'carousel_image_2' => ['required', 'max:2048', 'file', 'image', 'mimes:png,jpg', 'mimetypes:image/jpg,image/png', 'regex:/^(.*)(\.jpg|\.png)$/i'],
+            'carousel_image_3' => ['required', 'max:2048', 'file', 'image', 'mimes:png,jpg', 'mimetypes:image/jpg,image/png', 'regex:/^(.*)(\.jpg|\.png)$/i'],
             'ninja_product_title' => ['required', 'string', 'max:250', 'min:10', 'alpha', 'regex:/^[a-zA-Z1-9]+$/'],
             'ninja_product_detail' => ['required', 'string', 'max:50000', 'min:500', 'regex:/^[a-zA-Z0-9$]+$/|string'],
             'ninja_product_shiping_detail' => ['required', 'string', 'max:50000', 'min:500', 'regex:/^[a-zA-Z0-9$]+$/|string'],
-            'ninja_product_price' => [],
-            'ninja_product_shiping_price' => []
+            'ninja_product_price' => ['required', 'numeric', 'between:1, 9'],
+            'ninja_product_shiping_price' => ['required', 'numeric', 'between:1, 9']
         ]);
         $product->thumnail_image   =   $request->ninja_thumnail_image;
         $product->product_image_1  =   $request->carousel_image_1;
